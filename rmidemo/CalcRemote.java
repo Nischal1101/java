60c8f20f5488215b5713b6e8aa7dbfca508a8e4e
nischal1101 <kaflenk123@gmail.com>
2024-12-20 12:35:48 +0545

    Implementing remote interface

---
+++ rmidemo/CalcRemote.java
@@ -0,0 +1,15 @@
+package rmidemo;
+
+import java.rmi.*;
+import java.rmi.server.*;
+
+public class CalcRemote extends UnicastRemoteObject implements Calc {
+
+    public CalcRemote() throws RemoteException {
+        super();
+    }
+
+    public int multiply(int a, int b) {
+        return (a * b);
+    }
+}
