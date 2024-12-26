54127e7de89067491cc72c8a78043d648b67130d
nischal1101 <kaflenk123@gmail.com>
2024-12-26 08:31:55 +0545

    add file write

--- file/ByteStream.java
+++ file/ByteStream.java
@@ -2,30 +2,27 @@ package file;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ByteStream {
- public static void main(String[] args) throws FileNotFoundException,IOException
- {
-     try 
-     {
-      FileInputStream fis = new FileInputStream("./file/ball.txt"); 
-         int i=0;
-         while((i=fis.read())!=-1)
-         {
-             System.out.print((char)(i));
-         }
-         fis.close();
-        }
- 
-        catch (FileNotFoundException e)
-          {
-            System.out.println(e.getMessage());
-          }
-        catch(IOException e)
-          {
 
-          System.out.println(e.getMessage());
-          }
-  }
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        try {
+            FileInputStream fis = new FileInputStream("./file/ball.txt");
+            FileOutputStream fos = new FileOutputStream("./file/mmall.txt");
+            String hello = "hi";
+            fos.write(hello.getBytes());
+            int i = 0;
+            while ((i = fis.read()) != -1) {
+                System.out.print((char) (i));
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
+        }
+    }
 }
