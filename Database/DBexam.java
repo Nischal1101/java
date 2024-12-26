45f56ba32d0627c9937e895e114f7c52c97a23ed
nischal1101 <kaflenk123@gmail.com>
2024-12-26 21:23:47 +0545

    add scanner

--- Database/DBexam.java
+++ Database/DBexam.java
@@ -1,28 +1,26 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBexam {
- public static void main(String[] args) {
-  try
-  {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
-        conn.close();
-        String sql="";
-        PreparedStatement pstmt=conn.prepareStatement(sql);
-        pstmt.setString("name","nischal");
-        pstmt.executeQuery();
 
-  }
-  catch(SQLException se)
-  {
-    System.out.println(se.getMessage());
- } 
- catch(Exception e)
- {
-  System.out.println(e.getMessage());
- }
-}
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+            conn.close();
+            String sql = "INSERT INTO students (name,id) VALUES(?,?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "nischal");
+            pstmt.setInt(2, 1);
+            pstmt.executeQuery();
+
+        } catch (SQLException se) {
+            System.out.println(se.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- basics/ForEach.java
+++ basics/ForEach.java
@@ -1,9 +1,14 @@
-public class ForEach{
-  public static void main(String args[]){
-    int[] a={1,2,3,4,5};
-    for(int i:a)
-    {
-      System.out.println(i);
+
+import java.util.*;
+
+public class ForEach {
+
+    public static void main(String args[]) {
+        int[] a = {1, 2, 3, 4, 5};
+        for (int i : a) {
+            Scanner ins = new Scanner(System.in);
+            System.out.println(ins.nextLine());
+            System.out.println(i);
+        }
     }
-  }
 }
